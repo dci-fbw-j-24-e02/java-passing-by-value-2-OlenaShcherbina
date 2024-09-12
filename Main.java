1d146ebcbe7d1978d34d9d5ec78873f07b46006f
OlenaShcherbina <olenas.shcherbina@gmail.com>
2024-09-12 14:22:38 +0200

    added input validation and exception handling

--- Main.java
+++ Main.java
@@ -6,10 +6,16 @@ public class Main{
 
         Main main = new Main();
         Scanner scan = new Scanner(System.in);
-
+        int number;
+do {
+    try {
         System.out.println("Please enter the number: ");
-        int number = scan.nextInt();
-
+        number = Integer.valueOf(scan.nextLine().trim());
+        break;
+    }catch(NumberFormatException e){
+        System.out.println("It's not a number");
+    }
+}while(true);
         System.out.println("Number = " + number);
         main.increment(number);
         System.out.println("Number after calling the increment method = " + number);
